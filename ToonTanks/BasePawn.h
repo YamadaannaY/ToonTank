e877a8b3e220970db4fc33bb683665cb3d4b97ea
YamadaannaY <jiangjyjiang@gmail.com>
2025-10-22 15:50:31 +0800

    更新 BasePawn.h注释

--- ToonTanks/BasePawn.h
+++ ToonTanks/BasePawn.h
@@ -15,35 +15,47 @@ public:
 	// Sets default values for this pawn's properties
 	ABasePawn();
 
+	//处理销毁事件
 	void HandeleDestruction();
 
 protected:
+	//向目标方向向量进行旋转
 	void RotateTurret(FVector LookAtTarget);
+	//攻击
 	void Fire();
 
 private:
+	//碰撞组件
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Components",meta=(AllowPrivateAccess=true));
 	class UCapsuleComponent* CapsuleComp;
 	
+	/**使用组件分离，使得炮台能够给予基体进行转向**/
+
+	//基体组件
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Components",meta=(AllowPrivateAccess=true));
 	UStaticMeshComponent* BaseMesh;
 	
+	//炮塔组件
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Components",meta=(AllowPrivateAccess=true));
 	UStaticMeshComponent* TurretMesh;
 	
+	//子弹产生地点
 	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category="Components",meta=(AllowPrivateAccess=true));
 	USceneComponent* ProjectilesSpawnPoint;
 
+	//自由选择子弹类下的子类
 	UPROPERTY(EditDefaultsOnly,Category="Combat")
-	//allow the use of subclass of class,such as it`s  blueprint class which have Mesh we need.
 	TSubclassOf<class AProjectile> ProjectileClass;
 
+	//死亡粒子特效
 	UPROPERTY(EditDefaultsOnly,Category="Combat")
 	class UParticleSystem* DeathParticle;
 
+	//死亡音效
 	UPROPERTY(EditDefaultsOnly,Category="Combat")
 	class USoundBase* DeathSound;
 
+	//死亡时相机震动，具体震动效果不同Pawn各自一个具体类
 	UPROPERTY(EditAnywhere,Category="Combat")
 	TSubclassOf<class UCameraShakeBase> DeathCameraShakeclass;
 
